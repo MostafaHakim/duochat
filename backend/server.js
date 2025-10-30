ed1ff9551f719c9ded56cfca27be770679f163b7
Md Mostafa <mdmostafahakim@gmail.com>
2025-10-30 18:02:30 +0600

    first commit

--- backend/server.js
+++ backend/server.js
@@ -140,4 +140,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => console.log("✅ Server running on port 5000"));
+server.listen(PORT, () => console.log("✅ Server running on port 5000"));
